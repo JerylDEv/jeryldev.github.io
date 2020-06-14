2ab5c7b44ff0a569241024705979e1d9e22a8953
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-06-14 13:39:52 +0800

    updated the about Data

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,9 +19,9 @@ export const heroData = {
 export const aboutData = {
   img: 'profile3.jpg',
   paragraphOne:
-    "I have a bachelor's degree in Accounting and have been in the accounting and software services industry for more than 8 years. I currently work as a Technical Support Engineer assisting NetSuite users.",
+    "I graduated with a bachelor's degree in Accounting and have been in the accounting and software services industry for more than 8 years. I'm currently working for a company as a Technical Support Engineer assisting NetSuite users.",
   paragraphTwo:
-    'In 2018, I decided to pursue my old dream of becoming a Full Stack Developer. I made a commitment to learn how to code every day and also enrolled in a Graduate Diploma in Computer Science program in a university. In 2019, I published my first Flutter mobile application, created a couple of Flutter mobile application prototypes, created videos about Flutter and the Dart programming language, and trained a group of developers on how to create Flutter applications.',
+    'In 2018, I decided to pursue my old dream of becoming a Full Stack Developer. I made a commitment to learn how to code every day and also enrolled in a Graduate Diploma in Computer Science program in a university. In 2019, I published my first Flutter mobile app, created a couple of Flutter mobile application prototypes, launched a series of videos teaching Flutter and the Dart programming language, and trained a group of developers on how to create Flutter applications.',
   paragraphThree:
     'On May 16, 2020, I finally earned my Full Stack, Developer Certification at freeCodeCamp.org after completing more than 1800 hours of coursework and submitting 30 projects ranging from "Responsive Web Design" to "Information Security and Quality Assurance". I am regularly building my side-projects and explore different web development stacks outside of work.',
   resume: 'https://drive.google.com/file/d/177trGqcBbYOy9ow2-fR9qu68Xx-lctpg/view', // if no resume, the button will not show up
